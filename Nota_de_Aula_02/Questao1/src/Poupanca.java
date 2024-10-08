7e854fd4699ef3dfe6ba574b51b5a3d11a4be08c
VictorSales02 <victor.ferreira.sales.06@gmail.com>
2024-10-07 21:39:25 -0300

    Add files via upload

---
+++ Nota_de_Aula_02/Questao1/src/Poupanca.java
@@ -0,0 +1,23 @@
+public class Poupanca extends Conta {
+    public Poupanca(String titular) {
+        super(titular);
+    }
+
+    public void calcularRendimento(double taxa) {
+        double selic = obterTaxaSelic();
+        double rendimento;
+
+        if (selic > 8.5) {
+            rendimento = 0.005 * saldo;
+        } else {
+            rendimento = 0.007 * selic * saldo;
+        }
+
+        saldo += rendimento;
+        System.out.println("Rendimento calculado com sucesso!");
+    }
+
+    private double obterTaxaSelic() {
+        return 6.7;
+    }
+}
